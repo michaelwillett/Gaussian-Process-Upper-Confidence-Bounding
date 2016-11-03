8e09f96f0bfbf789167e8a120374292ff5fab424
Michael Willett <michaelwillett@users.noreply.github.com>
2016-11-03 12:24:44 -0400

    something resembling working code

---
+++ BlackBox.py
@@ -0,0 +1,37 @@
+import numpy as np
+import matplotlib
+
+class BlackBox1D:
+    def __init__(self):
+        pass
+    
+    @staticmethod
+    def FunctionA(x):
+        return (1+np.sin(.5*np.sin(x-6)*x + 4))*(-x*x/3 + 4*x - 1)+np.random.normal(0, .05, 1)
+    
+    @staticmethod
+    def Sinc(x):
+        sc = 1
+        if (x != 4):
+            sc = np.sin(x-4)/(x-4)
+            
+        return  5*sc + np.random.normal(0, .1, 1)
+
+
+
+class BlackBox2D:
+    def __init__(self):
+        pass
+    
+    @staticmethod
+    def FunctionA(x):
+        return BlackBox1D.FunctionA(x[0]) + BlackBox1D.Sinc(x[1])
+    
+    
+class BlackBoxND:
+    def __init__(self):
+        pass
+    
+    @staticmethod
+    def sinND(x):
+        return sum(np.sin([i/2 for i in x])) + np.random.normal(0, .05, 1)
--- GP_UCB.py
+++ GP_UCB.py
@@ -4,58 +4,208 @@ Created on Oct 1, 2016
 @author: Michael Willett
 '''
 import pyGPs
+import itertools as it
 import numpy as np
-import BlackBox1D
+import scipy.optimize as opt
+import matplotlib.pyplot as plt
+import mpl_toolkits.mplot3d
+import BlackBox
 import time
 
-if __name__ == '__main__':
-    sample = BlackBox1D.FunctionA()
-    
-    br = False
-    class domain:
-        def __init__(self):
-            pass
-     
-    d = domain()
+class GP_UCB:
+    def __init__(self):
+        self.func = None
+        self.model = None
+        self.domain = []
+        self.dim = 0
+        self.max_t = 0
         
-    d.min = [0]
-    d.max = [10]
-    
-    x = np.array([sum(i)/2. for i in zip(d.min,d.max)])
-    y = np.array(sample.evaluate(x))
-    z = np.linspace(d.min[0],d.max[0])
-    
-    
-    model = pyGPs.GPR()      # specify model (GP regression)
-    k = pyGPs.cov.Matern()
-    m = pyGPs.mean.Linear()
-    
-    converged = False
-    t = 1
-    delta = .5
-    
-    #for i in range(0, 10):
-    while not converged:
-        model.getPosterior(x, y) # fit default model (mean zero & rbf kernel) with data
-        model.optimize(x, y)     # optimize hyperparamters (default optimizer: single run minimize)
-        model.predict(z)         # predict test cases
+    def minfunc(self,x):
+        self.model.predict(np.array([x]))
+        t = self.regret.size
+        t = 1
         
-        mu_t = [item for sublist in model.ym for item in sublist] # compress from list of 1-element lists to list
-        s2_t = [item for sublist in model.ys2 for item in sublist] # compress from list of 1-element lists to list
+        # Get information gain
+        if model.kernel == 'Linear':
+            self.IG = model.dim * np.sqrt(t)
+            
+        if model.kernel == 'RBF':
+            self.IG = np.sqrt(t*pow(np.log(t),self.dim+1))
+            
+        if model.kernel == 'Matern':
+            v = 1.5
+            e = (v + model.dim*(model.dim + 1)) / (2*v + model.dim*(model.dim + 1))
+            self.IG = pow(t, e)
+
+        delta = .9      # delta \in (0,1): the lower this is, the more we explore uncertainty
+        normD = np.linalg.norm([x[1]-x[0] for x in self.domain])
+        self.beta_t = 2*np.log(self.dim*t*t*np.pi*np.pi/(6*delta)) 
         
-        beta_t = 2*np.log(t*t*np.pi*np.pi/(6*delta))
+        #self.beta_t = self.IG*pow(np.log(t/delta),3)
         
-        rng = [a+b*beta_t for a,b in zip(mu_t, s2_t)]
-        x_t = z[np.argmax(rng)]
+        mu_t = [item for sublist in self.model.ym for item in sublist] # compress from list of 1-element lists to list
+        s2_t = [item for sublist in self.model.ys2 for item in sublist] # compress from list of 1-element lists to list
+                    
+        retv = -1*(mu_t[0]+s2_t[0]*np.sqrt(self.beta_t))
+        return  retv
+    
+    def UCB(self): 
+        x0 = [[sum(n)/2 for n in self.domain]]
         
-        converged = (x_t == x[-1]) or (t > 100)
-        t += 1
-        print x_t, "<", t, ">"
+        params = {'approx_grad':True} 
+        kwargs = {"method": "TNC", "bounds": self.domain, "options":params}
+        rslt = opt.basinhopping(self.minfunc, x0, minimizer_kwargs=kwargs, niter=3)
+        
+        return rslt.x
+    
+    def TestConvergence(self, x):
+        #return  (np.linalg.norm(x[-1]-x[-2]) < .001)
+        return self.regret.size > model.max_t
+    
+    
+    def Plot2D(self, x, reset = False):
+        self.model.predict(self.z)
+        
+        mp = np.reshape(self.model.ym, (self.res[0], self.res[1]))
+        up = np.reshape(self.model.ym + self.model.ys2, (self.res[0], self.res[1]))
+        dp = np.reshape(self.model.ym - self.model.ys2, (self.res[0], self.res[1]))
+        xp = np.reshape(self.z[:,0], (self.res[0], self.res[1]))
+        yp = np.reshape(self.z[:,1], (self.res[0], self.res[1]))
                 
+        self.surf = self.ax.plot_surface(xp, yp, mp, color='blue', zorder=0, alpha=0.5)
+        self.up = self.ax.plot_wireframe(xp, yp, up, color='green', rstride=5, cstride=5, zorder=1)
+        self.dp = self.ax.plot_wireframe(xp, yp, dp, color='green', rstride=5, cstride=5, zorder=2)
         
-        x = np.append(x, x_t)
-        y = np.append(y, sample.evaluate(x_t))
+        z = self.model.predict(np.reshape(x, [1,2]))
+        self.pt = self.ax.scatter(x[0], x[1], z[0], marker='v', c='red', s=80, zorder=3)
+        plt.pause(0.001)
         
+        if reset:
+            self.surf.remove()
+            self.up.remove()
+            self.dp.remove()
+            self.pt.remove()
+        else:
+            plt.ioff()
+            plt.show()
+            
+    def PlotRegret(self, reset = False):
         
-    print "estimated max: ", x[-1]
-    model.plot()             # and plot result
+        T = self.regret.size+1
+
+        s2 = [item for sublist in self.model.ys2 for item in sublist]
+        isg = 1/s2[0] 
+        regretBound = np.sqrt(T*self.beta_t*self.IG*8/np.log(1+isg))
+        
+        self.regretBound = np.append(self.regretBound, regretBound)
+        
+        self.regplt = plt.plot(self.regret, color='b')
+        #self.boundplt = plt.plot(self.regretBound, color='r')
+        plt.pause(0.001)
+        
+        if reset:
+            self.regplt.pop(0).remove()
+            #self.boundplt.pop(0).remove()
+        else:
+            plt.ioff()
+            plt.show()
+            
+    def PrintStatus(self, modelTime, gradTime):
+        l = self.regret.size
+        totalTime = round(modelTime + gradTime, 4)*1000
+        ratio = round(gradTime*1000 / totalTime, 3)*100
+        formatPoint = [ round(x, 4) for x in list(self.x[l-1]) ]
+        
+        print "Iteration " + `l` + ":"
+        print "    sample point:  " + `formatPoint`
+        print "    regret:        " + `round(self.regret[l-1]-self.regret[l-2],4)`
+        print "    runtime:       " + `totalTime` + \
+              "ms (decent: " + `ratio` + "%)"
+    
+    def Init1D(self):
+        self.func = BlackBox.BlackBox1D.FunctionA
+        self.domain = [(0, 10)]
+        self.dim = 1
+        self.max_t = 1000
+        self.res = [200]
+        self.z = np.array(np.linspace(self.domain[0][0], self.domain[0][1], self.res[0]))
+    
+    def Init2D(self):
+        self.func = BlackBox.BlackBoxND.sinND
+        self.domain = [(0, 10), (0, 10)]
+        self.dim = 2
+        self.max_t = 1000
+        self.res = [100, 100]
+        x = [np.linspace(self.domain[i][0], self.domain[i][1], self.res[i]) for i in range(self.dim)]
+        self.z = np.array(list(it.product(x[0], x[1])))
+                
+        plt.ion()
+        fig = plt.figure()
+        self.ax = fig.add_subplot(111, projection='2d')
+        
+    def InitND(self, kernel='Linear'):
+        self.func = BlackBox.BlackBoxND.sinND
+        self.domain = [(0, 10), (0, 10), (0, 10), (0, 10), (0, 10), (0, 10), (0, 10), (0, 10)]
+        self.optima = [3.1416, 3.1416, 3.1416, 3.1416, 3.1416, 3.1416, 3.1416, 3.1416]
+        self.dim = 5
+        self.max_t = 5000
+        self.res = [11, 11, 11, 11, 11, 11, 11, 11]
+        x = [np.linspace(self.domain[i][0], self.domain[i][1], self.res[i]) for i in range(self.dim)]
+        self.z = np.array(list(it.product(x[0], x[1], x[2])))
+        self.kernel = kernel
+        
+        plt.ion()
+        plt.figure()
+    
+    def GetOptima(self):
+        converged = False
+        
+        # initialize search space
+        self.x = np.array([[sum(n)/2 for n in self.domain]])
+        self.y = np.array([self.func(self.x[0])])
+        self.regret = np.array([np.linalg.norm(self.x[0] - self.optima)])
+        self.regretBound = np.array([1])
+        
+        # specify model (GP regression)
+        self.model = pyGPs.GPR()
+        m = pyGPs.mean.Zero()
+        k = pyGPs.cov.Linear()
+        if model.kernel == 'RBF':
+            k = pyGPs.cov.RBF()
+            
+        if model.kernel == 'Matern':
+            k = pyGPs.cov.Matern()
+        
+        self.model.setPrior(mean=m, kernel=k)
+        
+
+        #self.model.setNoise(0)
+        
+        while not converged:
+            start = time.time()
+            rslt = self.model.getPosterior(self.x, self.y, False) # fit default model (mean zero & rbf kernel) with data
+            self.model.optimize(self.x, self.y)     # optimize hyperparamters (default optimizer: single run minimize)
+            end = time.time()
+            
+            self.post = rslt[1]
+            
+            x_t = self.UCB()
+            self.regret = np.append(self.regret, np.linalg.norm(x_t - self.optima) 
+                                    + self.regret[self.regret.size-1])
+
+            
+            self.x = np.append(self.x, np.array([x_t]), axis=0)
+            self.y = np.append(self.y, self.func(x_t))
+            
+            converged = self.TestConvergence(self.x)
+            self.PrintStatus(end-start, time.time()-end)
+            self.PlotRegret(True)
+
+        
+        self.PlotRegret(False)
+        #self.Plot2D(x_t)
+        
+if __name__ == '__main__':
+    model = GP_UCB()
+    model.InitND('RBF')
+    model.GetOptima()
